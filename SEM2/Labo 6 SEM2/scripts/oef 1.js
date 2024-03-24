631160ea987598ac83412ec45fe4cfd64b2d9f4c
ThiboVerstraete <r0992020@student.vives.be>
2024-03-24 06:23:27 +0100

    test

--- SEM2/Labo 6 SEM2/scripts/oef 1.js
+++ SEM2/Labo 6 SEM2/scripts/oef 1.js
@@ -1,5 +1,5 @@
 const setup = () => {
-const paragraaf = document.querySelector("p");
+    const paragraaf = document.querySelector("p");
     paragraaf.removeChild(paragraaf.childNodes[0]);
     paragraaf.appendChild(document.createTextNode("Good Job!"));
 }
